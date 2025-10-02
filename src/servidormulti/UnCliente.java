cb1f8c11adcf808bb31d9be21eee6396b5dcd617
YazminGalvez <L22440144@mochis.tecnm.mx>
2025-10-02 07:45:07 -0700

    Initial commit

---
+++ src/servidormulti/UnCliente.java
@@ -0,0 +1,30 @@
+package servidormulti;
+
+import java.io.*;
+import java.net.Socket;
+
+public class UnCliente implements Runnable {
+
+    final DataOutputStream salida;
+    final BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+    final DataInputStream entrada;
+    UnCliente(Socket s) throws IOException {
+        salida = new DataOutputStream(s.getOutputStream());
+        entrada = new DataInputStream(s.getInputStream());
+    }
+
+    @Override
+    public void run() {
+        String mensaje;
+        while ( true ){
+            try {
+                mensaje = entrada.readUTF();
+                for ( UnCliente cliente : ServidorMulti.clientes.values()){
+                    cliente.salida.writeUTF(mensaje);
+                }
+            } catch (IOException ex) {
+
+            }
+        }
+    }
+}
