44d438543d28188399ec8829023f65e869306094
YazminGalvez <L22440144@mochis.tecnm.mx>
2025-10-08 20:43:50 -0700

    Corregir duplicación de clientes en ServidorMulti

--- src/servidormulti/ServidorMulti.java
+++ src/servidormulti/ServidorMulti.java
@@ -9,18 +9,13 @@ public class ServidorMulti {
     public static HashMap<String, UnCliente> clientes = new HashMap<String, UnCliente>();
     public static void main(String[] args) throws IOException {
         ServerSocket servidorSocket = new ServerSocket(8080);
-        int contador = 0;
         System.out.println("Servidor iniciado y esperando clientes...");
 
         while (true) {
             Socket s = servidorSocket.accept();
             UnCliente unCliente = new UnCliente(s);
             Thread hilo = new Thread(unCliente);
-            clientes.put(Integer.toString(contador), unCliente);
             hilo.start();
-            contador++;
         }
     }
 }
-
-
